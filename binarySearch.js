4ff8458b4000ee9131768f6969c8f9c4da6af1d9
Harrison <harrisonxm@gmail.com>
2017-08-15 15:33:21 -0700

    added recursive bsearch

--- binarySearch.js
+++ binarySearch.js
@@ -24,3 +24,36 @@ let primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 let result = binarySearch(primes, 73)
 
 console.log(result)
+
+
+
+
+//recursive binarySearch
+const binaryRSearch = function(array,target){
+  let length = array.length;
+  let mid = Math.floor(length/2);
+  let newArr = array;
+
+  if(newArr[mid] === target){
+    return true;
+  }
+  else if(length <= 0){
+    return false;
+  }
+  else if(newArr[mid] < target){
+    newArr = array.slice(mid+1, array.length)
+    return binaryRSearch(newArr,target)
+  }
+  else if(newArr[mid] > target){
+    newArr = array.slice(0, mid)
+    return binaryRSearch(newArr, target)
+  }
+}
+
+
+let res = binaryRSearch(primes, 73)
+console.log(res)
+res = binaryRSearch(primes, 3)
+console.log(res)
+res = binaryRSearch(primes, 1)
+console.log(res)
