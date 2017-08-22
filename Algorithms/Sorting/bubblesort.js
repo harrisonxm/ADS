7a6ce4728b5023da3203327b24fdb68b6ebee019
Harrison <harrisonxm@gmail.com>
2017-08-22 15:21:04 -0700

    added bubble sort

--- Algorithms/Sorting/bubblesort.js
+++ Algorithms/Sorting/bubblesort.js
@@ -18,3 +18,13 @@ let bubbleSort = function(arr){
 }
 
 console.log(bubbleSort([2,3,4,5,6,7]))
+
+//steps through element by element swaps if they are in wrong order;
+// then loops over again and does same process;
+
+
+//Big O
+// worst-case O(n^2)
+// Best-case O(n) -- already sorted, still have to iterate across once;
+//Average O(n^2)
+//worst-case space O(1) -- sorting done in place, not creating new list;
