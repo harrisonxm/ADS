d72070e6d835c02aab2621116bc3a6656bf058d8
Harrison <harrisonxm@gmail.com>
2017-08-22 17:41:57 -0700

    added insertion sort

--- Algorithms/Sorting/insertionsort.js
+++ Algorithms/Sorting/insertionsort.js
@@ -2,13 +2,13 @@ let insertionSort = function(arr){
   let key, j;
   for(let i=1; i < arr.length; i++){
     key = arr[i];
-    j = i-1;
+    j = i;
 
-    while(j >= 0 && key < arr[j]){
+    while(j >= 0 && key < arr[j-1]){
       arr[j+1] = arr[j];
       j--;
     }
-    arr[j+1] = key;
+    arr[j] = key;
   }
   return arr;
 }
@@ -21,5 +21,10 @@ console.log(insertionSort([5,2,1,5,7,9,10]));
 //Best: O(n) iterates one time to for sorted array
 
 
-//1.iterate through array
-//2.
+//1."assume" first element sorted
+//2. for loop through elements
+//3. set a variable to current index of array to compare - var key
+//4. also need to set a variable to the represent the "hole" we are going to create - var j
+//5. while the current index value of array is less than the elements that preceded it,
+// increment preceding elements greater than current, by one position forward.
+//6. now set the correct position for key
