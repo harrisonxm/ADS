7d09102bcbe93a92228b2999835bb5931050fc15
Harrison <harrisonxm@gmail.com>
2017-08-24 01:53:28 -0700

    added more on heap

--- Heaps/BuildHeap.js
+++ Heaps/BuildHeap.js
@@ -1,33 +1,34 @@
 
 let buildMaxHeap = function(arr){
-  arr.heapsize = arr.length;
-  let nonleaf = Math.floor(arr.length/2);
-  for(let i = nonleaf; i > 0; i--){
-    maxHeapify(arr, i);
+  let hsize = arr.length;
+  let nonleaf = Math.floor(arr.length/2)-1;
+  for(let i = nonleaf; i > -1; i--){
+    maxHeapify(arr,hsize,i);
   }
+  return arr
 }
 
-let maxHeapify = function(a,idx){
-  let left = 2 * idx;
-  let right = 2 * idx + 1;
+let maxHeapify = function(a,hsize,idx){
+  let left = 2 * idx + 1;
+  let right = 2 * idx + 2;
   let largest;
-  if(left <= a.heapsize && a[left] > a[idx]){
+  if(left <= hsize && a[left] > a[idx]){
     largest = left;
   } else {
     largest = idx;
   }
-  if(right <= a.heapsize && a[right] > a[largest]){
+  if(right <= hsize && a[right] > a[largest]){
     largest = right;
   }
   if(largest != idx){
     [a[idx], a[largest]] = [a[largest], a[idx]];
-    maxHeapify(a,largest)
+    maxHeapify(a,hsize,largest)
   }
 }
 
 let arr1 = [5,2,10,12,20,15,19,20];
 let heap = buildMaxHeap(arr1);
-console.log(heap.size)
+console.log(heap);
 
 //Almost complete BST properties:
 //non leaves - 1 to floor(n/2);
