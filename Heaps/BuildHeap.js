413dab8d6f00ed38fd46ba72a2e1e6a422362197
Harrison <harrisonxm@gmail.com>
2017-08-25 16:46:51 -0700

    minheapsort

--- Heaps/BuildHeap.js
+++ Heaps/BuildHeap.js
@@ -5,7 +5,6 @@ let buildMaxHeap = function(arr){
   for(let i = nonleaf; i > -1; i--){
     maxHeapify(arr,hsize,i);
   }
-  return arr
 }
 
 let maxHeapify = function(a,hsize,idx){
@@ -23,8 +22,23 @@ let maxHeapify = function(a,hsize,idx){
     maxHeapify(a,hsize,largest)
   }
 }
-
-let heapSort = function(arr){
+let minHeapify = function(a,hsize,idx){
+  let left = 2 * idx + 1;
+  let right = 2 * idx + 2;
+  let smallest = idx;
+  if(left < hsize && a[left] < a[idx]){
+    smallest = left;
+  }
+  if(right < hsize && a[right] < a[smallest]){
+    smallest = right;
+  }
+  if(smallest != idx){
+    [a[idx], a[smallest]] = [a[smallest], a[idx]];
+    minHeapify(a,hsize,smallest)
+  }
+}
+//using max heapify
+let heapSort1 = function(arr){
   let hsize = arr.length;
   let nonleaf = Math.floor(arr.length/2)-1;
 
@@ -38,9 +52,25 @@ let heapSort = function(arr){
   }
 }
 
+//using min heapify
+let heapSort2 = function(arr){
+  let hsize = arr.length;
+  let nonleaf = Math.floor(arr.length/2)-1;
+
+  for(let i = nonleaf; i > -1; i--){
+    minHeapify(arr,hsize,i);
+  }
+
+  for(let i=hsize-1; i > 0; i--){
+    [arr[i], arr[0]] = [arr[0], arr[i]];
+    minHeapify(arr,i,0);
+  }
+}
+
 let arr1 = [5,2,10,12,20,15,19,20];
-heapSort(arr1)
+heapSort2(arr1)
 console.log(arr1)
+
 
 //Almost complete BST properties:
 //non leaves - 1 to floor(n/2);
