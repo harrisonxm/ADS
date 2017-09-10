48a64c58c41fc2a9c2cdf5a74491e850f0edc9cb
Harrison <harrisonxm@gmail.com>
2017-09-10 16:19:26 -0700

    string to int merge sorted list

---
+++ Algorithms/MergeTwoSortedLists.js
@@ -0,0 +1,45 @@
+class listNode {
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+function mergeSortedLists(l1,l2){
+  let dummy = new listNode(0);
+  let head = dummy;
+  while(l1 && l2){
+    if(l1.val < l2.val){
+      dummy.next = l1;
+      dummy = dummy.next;
+      l1 = l1.next;
+    }
+    else{
+      dummy.next = l2;
+      dummy = dummy.next;
+      l2 = l2.next;
+    }
+  }
+  if(l1){
+    dummy.next = l1;
+  }
+  else{
+    dummy.next = l2;
+  }
+  return head.next;
+}
+
+let i1 = new listNode(2)
+let i2 = new listNode(5)
+let i3 = new listNode(7)
+let i4 = new listNode(3)
+let i5 = new listNode(11)
+
+i1.next = i2;
+i2.next = i3;
+i4.next = i5;
+let res = mergeSortedLists(i1, i4);
+console.log(res)
+console.log(res.next)
+console.log(res.next.next)
+console.log(res.next.next.next)
---
+++ Algorithms/convertStringandInt.js
@@ -0,0 +1,36 @@
+function intToString(int){
+  let i = [];
+  while(true){
+      i.push(String.fromCharCode('0'.charCodeAt(0) + int % 10));
+      int = Math.floor(int/10);
+      if(int < 1){
+        break;
+      }
+  }
+  return i.reverse().join('')
+}
+
+
+function stringToInt(str){
+  let negative = str[0] === '-' ?  true : false;
+  str = negative ? str.slice(1) : str
+
+  let int = [];
+  let exp = str.length-1
+
+  for(var i = 0; i < str.length; i++){
+    int.push((str[i].charCodeAt(0) - 48) * Math.pow(10, exp))
+    exp--
+  }
+  let num = int.reduce((sum, i) => {
+    return sum + i;
+  }, 0)
+  if(negative === true){
+    return -num;
+  }
+  return num;
+}
+
+
+// let string = '-12345'
+// console.log(stringToInt(string))
